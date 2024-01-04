678fe7c89bc5d1f796b9299bc6d880dc79b0268c
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-01-04 12:56:22 +0300

    Solve task 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(-?\d{10,10})|(-?\d\:){4}-?\d/).join(,)
+puts ARGV[0].scan(/(?<=\[from:)(\w*)(?=\])|(?<=\[to:)(\w*)(?=\])|(?<=\[flags:)([-:\w]*)(?=\])/).join(,)
