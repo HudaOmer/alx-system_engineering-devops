c48b4b7dc81328df566cddebef1c9f88c38e0b10
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-24 04:59:26 +0300

    Solve 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
-# Kill a process
-exec {'pip3 install flask':
-  path => '/usr/bin:/usr/sbin:/bin',
+# Install flask version 2.1.0
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
