89259aa16e6e73b18fdf2cf5a1d037b7a7969020
HudaOmer <hudaoe175@gmail.com>
2024-03-04 16:05:30 -0800

    Solve Advanced 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+# This puppet script automats the creation of a custom HTTP header response
+# Same as task 0
+
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
