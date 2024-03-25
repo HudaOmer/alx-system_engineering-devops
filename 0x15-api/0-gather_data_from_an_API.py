8ba4dc8d125e6d6414749dba3774521e2047d0ae
HudaOmer <hudaoe175@gmail.com>
2024-03-25 16:14:59 -0700

    Add files

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Python script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODO list progress
+"""
+import requests
+
+x = requests.get('')
+
+print(x.text)
---
+++ 0x15-api/README.md
@@ -0,0 +1,3 @@
+# Project 0x15-api
+- Solutions By Huda Omer
+## 4 tasks
